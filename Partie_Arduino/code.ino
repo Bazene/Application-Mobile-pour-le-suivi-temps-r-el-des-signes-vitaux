a59fff1befb474f5ad2c93cc9ade4f01d1b61015
BAZENE SERGE Amos <bazenesergeamos0@gmail.com>
2024-02-01 08:51:50 +0100

    fichier qui conctient que le code pour capter la température avec le LM35

---
+++ Partie_Arduino/code.ino
@@ -0,0 +1,26 @@
+// Mesure de la température
+
+int LM35 = A1; //broche pour le capteur de la température
+int LM35_value;
+float temperature_enC; // Variable pour stocker la température en degrés Celsius
+
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  LM35_value = analogRead(LM35);// Lecture de la valeur analogique du capteur LM35
+  Serial.print("Valeur lue par le capteur : ");
+  Serial.println(LM35_value);
+
+  
+      float voltage = LM35_value * (5.0 / 1023.0); // Conversion de la valeur lue en tension (5V est la tension de référence de l'Arduino)
+      temperature_enC = (voltage * 100.0); // Calcul de la température en degrés Celsius
+    
+      Serial.print("Temperature: "); 
+      Serial.print(temperature_enC);
+      Serial.println(" celc"); 
+
+  delay(3000);
+}
