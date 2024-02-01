37aca3db08dd7fefc6669d39addd1da3ea89a9cc
BAZENE SERGE Amos <bazenesergeamos0@gmail.com>
2024-02-01 08:34:46 +0100

    menu BottomNavigationView avec ses fragments respectif

---
+++ MoyoSafi_app/app/src/main/java/com/course/android/ct/moyosafiapp/MainActivity.java
@@ -0,0 +1,44 @@
+package com.course.android.ct.moyosafiapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.course.android.ct.moyosafiapp.databinding.ActivityMainBinding;
+
+public class MainActivity extends FragmentActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot()); // link between MainActivity.java et activity_main.xml
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            if(item.getItemId() == R.id.home){
+                replaceFragment(new HomeFragment());
+            } else if (item.getItemId() == R.id.notifications) {
+                replaceFragment(new NotificationsFragment());
+            } else if (item.getItemId() == R.id.settings) {
+                replaceFragment(new SettingsFragment());
+            }
+
+            return true;
+        });
+    }
+
+    public void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+}
